72e7018f46932b58c25486afb1885f6eb587eff3
yujin shin <79908872+shinyujin97@users.noreply.github.com>
2023-12-24 19:30:46 +0900

    Update WebMvcConfiguration.java

--- src/main/java/com/javalab/boot/config/WebMvcConfiguration.java
+++ src/main/java/com/javalab/boot/config/WebMvcConfiguration.java
@@ -8,19 +8,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
     @Configuration
     public class WebMvcConfiguration implements WebMvcConfigurer {
 
-   @Override
+  /* @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // 노트북
         // 로컬 버전
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("file:///C:/JAVA/javaworks/spring-project/FlexshMallProject/files/");
-    }
+    }*/
 
-/*    // 클라우드 타입 버전
+    // 클라우드 타입 버전
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/files/**")
                 .addResourceLocations("file:///app/files/");
-    }*/
+    }
 }
 
